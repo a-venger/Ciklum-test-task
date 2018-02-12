b4a02b018cb5072a5cb1aad780692abb2662e164
Aleksandr Venger <dj_sanek1@meta.ua>
2018-02-12 14:17:11 +0200

    open html report after run

--- Task 1. Load testing part/runtest.bat
+++ Task 1. Load testing part/runtest.bat
@@ -1,1 +1,6 @@
-apache-jmeter-4.0\bin\jmeter.bat -n -t "testplan\Deezer.jmx" -Jusers=50 -Jrampup=300 -Jduration=900 -l "results\testResult.csv" -j "results\jmeter.log" -e -o "results\Html\"
+@ECHO OFF
+cd .\apache-jmeter-4.0
+@RD /S /Q "..\results"
+call bin\jmeter.bat -n -t "..\testplan\Deezer.jmx" -Jusers=50 -Jrampup=300 -Jduration=900 -l "..\results\testResult.csv" -j "..\results\jmeter.log" -e -o "..\results\Html" -f
+call start chrome ..\results\Html\index.html
+pause
