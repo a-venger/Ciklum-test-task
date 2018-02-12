56f7ef12b0ab71ce1200ab53fb523849a6ee9899
Aleksandr Venger <dj_sanek1@meta.ua>
2018-02-12 14:29:33 +0200

    fix for reach

--- Task 1. Load testing part/runtest.bat
+++ Task 1. Load testing part/runtest.bat
@@ -1,6 +1,19 @@
-@ECHO OFF
 cd .\apache-jmeter-4.0
+set /p users=Enter number of users:
+set /p rampup=Enter rampup: 
+set /p duration=Enter duration: 
 @RD /S /Q "..\results"
-call bin\jmeter.bat -n -t "..\testplan\Deezer.jmx" -Jusers=50 -Jrampup=300 -Jduration=900 -l "..\results\testResult.csv" -j "..\results\jmeter.log" -e -o "..\results\Html" -f
+call bin\jmeter.bat -n -t "..\testplan\Deezer.jmx" -Jusers=%users% -Jrampup=%Jrampup% -Jduration=%duration% -l "..\results\testResult.csv" -j "..\results\jmeter.log" -e -o "..\results\Html" -f
+
+SET LookForFile="..\results\Html\index.html"
+
+:CheckForFile
+IF EXIST %LookForFile% GOTO OpenReport
+TIMEOUT /T 5 >nul
+
+GOTO CheckForFile
+
+:OpenReport
 call start chrome ..\results\Html\index.html
+
 pause
